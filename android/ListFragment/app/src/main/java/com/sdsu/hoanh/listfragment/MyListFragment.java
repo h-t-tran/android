62d452ff956701af2b174850a5a79377545bb8f0
httran <hoanh@yahoo.com>
2015-02-07 02:07:00 -0800

    got listview selection highlited working

--- android/ListFragment/app/src/main/java/com/sdsu/hoanh/listfragment/MyListFragment.java
+++ android/ListFragment/app/src/main/java/com/sdsu/hoanh/listfragment/MyListFragment.java
@@ -1,5 +1,7 @@
 package com.sdsu.hoanh.listfragment;
 
+import android.app.Activity;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.widget.ArrayAdapter;
@@ -27,11 +29,47 @@ public class MyListFragment extends ListFragment {
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this.getActivity(),
                 //android.R.layout.simple_spinner_dropdown_item,
-                //android.R.layout.simple_list_item_1,
-                android.R.layout.simple_list_item_single_choice,
+                android.R.layout.simple_list_item_1,
+                //android.R.layout.simple_list_item_single_choice,
                 locationArray);
 
         this.setListAdapter(dataAdapter);
 
     }
+
+    @Override
+    public void onAttach(Activity act)
+    {
+        super.onAttach(act);
+
+    }
+
+    @Override
+    public void onActivityCreated(Bundle bundle)
+    {
+        super.onActivityCreated(bundle);
+
+        try {
+            ListView lv = this.getListView();
+            lv.setSelection(2);
+
+            Object o = lv.getSelectedItem();
+            lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+            int lightGreen = 0xA9BCF500;
+            ColorDrawable cd = new ColorDrawable(lightGreen);
+            lv.setSelector(cd);
+            lv.getAdapter();
+        }
+        catch(Exception e)
+        {
+            String m = e.getMessage();
+            String s = m;
+        }
+    }
+
+//    private static void setDefaultListSelector(ListView listView, Context context) {
+//        TypedArray typedArray = context.obtainStyledAttributes(null, new int[]{android.R.attr.listSelector}, android.R.attr.listViewStyle, 0);
+//        Drawable drawable = typedArray.getDrawable(0);
+//        listView.setSelector(drawable);
+//    }
 }
