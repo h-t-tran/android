6b0bd231a959558b40a593d6e75c06b459a7fdf6
httran <hoanh@yahoo.com>
2015-02-07 17:23:27 -0800

    got main act populated

--- android/Assignment2/app/src/main/java/com/sdsu/hoanh/assignment2/DesertFragment.java
+++ android/Assignment2/app/src/main/java/com/sdsu/hoanh/assignment2/DesertFragment.java
@@ -1,8 +1,60 @@
 package com.sdsu.hoanh.assignment2;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DesertFragment extends ListFragment {
+
+    private static final String _cupCake = "Cupcake";
+    private static final String _donut = "Donut";
+    private static final String _gingerBread = "Gingerbread";
+    private static final String _iceCream = "Ice Cream";
+    private static final String _jellyBean = "Jelly Bean";
+
+    @Override
+    public void onCreate(Bundle bundle)
+    {
+        super.onCreate(bundle);
+
+        getActivity().setTitle("My List Fragment");
+
+        List<String> dessertArray = new ArrayList<String>();
+        dessertArray.add(_cupCake);
+        dessertArray.add(_donut);
+        dessertArray.add(_gingerBread);
+        dessertArray.add(_iceCream);
+        dessertArray.add(_jellyBean);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this.getActivity(),
+                //android.R.layout.simple_list_item_1,
+                android.R.layout.simple_list_item_single_choice,
+                dessertArray);
+
+        this.setListAdapter(dataAdapter);
+
+    }
+
+
+    @Override
+    public void onActivityCreated(Bundle bundle)
+    {
+        super.onActivityCreated(bundle);
+
+        ListView lv = this.getListView();
+        lv.setSelection(2);
+
+        Object o = lv.getSelectedItem();
+        lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        int lightGreen = 0xA9BCF500;
+        ColorDrawable cd = new ColorDrawable(lightGreen);
+        lv.setSelector(cd);
+    }
 
 }
--- android/Assignment2/app/src/main/java/com/sdsu/hoanh/assignment2/MainActivity.java
+++ android/Assignment2/app/src/main/java/com/sdsu/hoanh/assignment2/MainActivity.java
@@ -4,15 +4,89 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String _dateEntryText = "Date Entry";
+    private static final String _keyboardEntryText = "Keyboard Entry";
+    private static final String _listSelText = "List Selection";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this._fillActivitySelectionSpinner();
+        if (savedInstanceState == null) {
+            _loadDesertListFragment();
+        }
     }
+
+
+    private void _fillActivitySelectionSpinner()
+    {
+        final Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        List<String> actArray = new ArrayList<String>();
+        actArray.add(_dateEntryText);
+        actArray.add(_keyboardEntryText);
+        actArray.add(_listSelText);
+
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                actArray);
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
+
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+//        {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position,
+//                                       long id) {
+//                Object obj = spinner.getSelectedItem();
+//                String label = parent.getItemAtPosition(position).toString();
+//            }
+//            @Override
+//            public void onNothingSelected(AdapterView<?> arg0) {
+//                // TODO Auto-generated method stub
+//
+//            }
+//        });
+
+    }
+
+    private void _loadDesertListFragment()
+    {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container_list_frag_host, new DesertFragment())
+                .commit();
+
+    }
+
+    private void _goToDateActivity()
+    {
+
+    }
+
+    private void _goToKeyboardActivity()
+    {
+
+    }
+
+    private void _goToListActivity()
+    {
+
+    }
+
 
 
     @Override
