adf54f13ee745551a8504530a33e0a000264b95e
httran <hoanh@yahoo.com>
2015-03-30 01:18:03 -0700

    show detail works

--- android/GeoAlbum/app/src/main/java/com/sdsu/hoanh/geoalbum/PictureDetailActivity.java
+++ android/GeoAlbum/app/src/main/java/com/sdsu/hoanh/geoalbum/PictureDetailActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.sdsu.hoanh.geoalbum.Model.Photo;
 import com.sdsu.hoanh.geoalbum.Model.PhotoDatabaseHelper;
@@ -16,12 +18,36 @@ public class PictureDetailActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_picture_detail);
+        setContentView(R.layout.activity_take_picture); //activity_picture_detail);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, _picDetailFagment)
                     .commit();
         }
+
+
+        Button acceptButton = (Button)this.findViewById(R.id._okPhoto);
+        acceptButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // save pic
+                if(PictureDetailActivity.this._picDetailFagment.savePhoto()) {
+
+                    // once saved return to main activity
+                    PictureDetailActivity.this.finish();
+                }
+            }
+        });
+
+        Button cancelButton = (Button)this.findViewById(R.id._cancelPhoto);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PictureDetailActivity.this.finish();
+            }
+        });
+
     }
 
 
